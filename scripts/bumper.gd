d61745ab31bd3d7d7c90290c97111a43144e0aa3
Antoine Vuillet <121035207+Antoine-Vuillet@users.noreply.github.com>
2025-11-19 10:13:07 +0100

    feat: boss arena and visibility of movement of boss arena added

--- scripts/bumper.gd
+++ scripts/bumper.gd
@@ -3,6 +3,5 @@ extends Node3D
 @export var strength : float = 10
 
 func bounce(body: Node3D):
-	print("Maybe?")
 	if(body.name == "Player"):
-		body.velocity.y = strength
+		body.velocity.y = max(strength,body.velocity.y)
