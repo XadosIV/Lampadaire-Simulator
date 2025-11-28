e40a6ddfb2ec2809f0dab7c6578edf81e90d927e
Xad <83866570+XadosIV@users.noreply.github.com>
2025-11-28 15:52:08 +0100

    bossLight

--- scripts/mesh_settings.gd
+++ scripts/mesh_settings.gd
@@ -2,10 +2,20 @@ extends Node3D
 
 @export var light = true
 @export var jaune = false
+@export var bossLight = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$OmniLight3D.visible = light
+	var l : OmniLight3D = $OmniLight3D
+	
+	l.visible = light
+	
+	if bossLight:
+		l.omni_range = 25
+		l.light_energy = 30
+	else:
+		l.omni_range = 5
+		l.light_energy = 2
 
 	if jaune:
 		var mat := StandardMaterial3D.new()
