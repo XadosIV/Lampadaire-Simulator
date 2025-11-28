ea432359b88bb2bd288c8d5289a73ff705b64977
Xad <83866570+XadosIV@users.noreply.github.com>
2025-11-28 15:25:44 +0100

    tkt

--- scripts/m_canonpadaire.gd
+++ scripts/m_canonpadaire.gd
@@ -3,7 +3,20 @@ extends CharacterBody3D
 signal shoot
 @onready var animplayer: AnimationPlayer = $AnimationPlayer
 
+@export var isDecoration = false
+var player
+
+func _ready():
+	player = get_node("/root/Map/Player")
+
+func _process(_delta):
+	if isDecoration:
+		look_at(player.position)
+
+
 func shooting(body:Node3D):
+	if isDecoration: return
+	
 	if (body.name == "Player"):
 		emit_signal("shoot")
 		animplayer.animation_get_next("shot")
