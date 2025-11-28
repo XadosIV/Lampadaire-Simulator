46f654e7f57f8ca40dcbc965bc5da0990a7e9aff
Xad <83866570+XadosIV@users.noreply.github.com>
2025-11-28 15:39:35 +0100

    correction

--- scripts/m_canonpadaire.gd
+++ scripts/m_canonpadaire.gd
@@ -2,19 +2,24 @@ extends CharacterBody3D
 
 signal shoot
 @onready var animplayer: AnimationPlayer = $AnimationPlayer
-@export var hasShot = false
+var hasShot = false
+@export var isDecoration = false
+
 
 var player
 
 func _ready():
 	player = get_node("/root/Map/Player")
+	if not player:
+		player = get_node("/root/BossArena/Player")
 
 func _process(_delta):
 	if isDecoration:
 		look_at(player.position)
 
-
 func shooting(body:Node3D):
+	if isDecoration: return
+	
 	if (body.name == "Player" && !hasShot):
 		emit_signal("shoot")
 		animplayer.play("shot")
