744460358d384e77b90428361316317470599573
SkyMind974 <greniersteven97410@gmail.com>
2025-12-09 13:51:43 +0100

    feat: popup

---
+++ scripts/Popup.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_quitter_pressed() -> void:
+	visible = false
+	get_tree().paused = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
--- scripts/boss_arena.gd
+++ scripts/boss_arena.gd
@@ -1,10 +1,22 @@
 extends Node3D
 
+@export var popup : Control
+var popup_done := false
 signal defog
 
 func move_to_center():
-	var center_pos = Vector3(0, 50, 0)
+	var center_pos = Vector3(20, 50, 10)
 	emit_signal("defog")
 	var tween = create_tween()
 	$Multipadaire.Hpbar.visible = true
 	tween.tween_property(self, "position", center_pos, 5.0)
+
+
+func _on_body_entered_popup(body: Node3D) -> void:
+	if popup_done:
+		return
+	if body.name == "Player":
+		popup_done = true
+		popup.visible = true
+		get_tree().paused = true
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
