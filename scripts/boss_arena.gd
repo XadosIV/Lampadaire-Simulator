51384028e0d30ab1bbe70e7b34baf8bf0ab34160
SkyMind974 <greniersteven97410@gmail.com>
2025-12-09 09:15:46 +0100

    fix: HP boss when arrive on boss arena

--- scripts/boss_arena.gd
+++ scripts/boss_arena.gd
@@ -6,4 +6,5 @@ func move_to_center():
 	var center_pos = Vector3(0, 50, 0)
 	emit_signal("defog")
 	var tween = create_tween()
+	$Multipadaire.Hpbar.visible = true
 	tween.tween_property(self, "position", center_pos, 5.0)
